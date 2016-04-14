1990f34b90e9f4e3f68d754de054a71aca5e21a2
nathanfaucett <nathanfaucett@gmail.com>
2016-04-13 19:43:37 -0500

    added new

--- src/create.rs
+++ src/create.rs
@@ -2,12 +2,16 @@ use num::Num;
 
 
 #[inline(always)]
-pub fn create<T: Num>(m11: T, m12: T, m13: T, m21: T, m22: T, m23: T) -> [T; 6] {
+pub fn new<T: Num>(m11: T, m12: T, m13: T, m21: T, m22: T, m23: T) -> [T; 6] {
     [m11, m12, m13, m21, m22, m23]
 }
+#[inline(always)]
+pub fn create<T: Num>(m11: T, m12: T, m13: T, m21: T, m22: T, m23: T) -> [T; 6] {
+    new(m11, m12, m13, m21, m22, m23)
+}
 #[test]
-fn test_create() {
-    let m = create(
+fn test_new() {
+    let m = new(
         1, 0, 0,
         0, 1, 0
     );
@@ -18,22 +22,22 @@ fn test_create() {
 }
 
 #[inline(always)]
-pub fn create_identity<T: Num>() -> [T; 6] {
-    create(
+pub fn new_identity<T: Num>() -> [T; 6] {
+    new(
         T::one(), T::zero(), T::zero(),
         T::zero(), T::one(), T::zero()
-    ) 
+    )
 }
 #[inline(always)]
-pub fn create_zero<T: Num>() -> [T; 6] {
-    create(
+pub fn new_zero<T: Num>() -> [T; 6] {
+    new(
         T::zero(), T::zero(), T::zero(),
         T::zero(), T::zero(), T::zero()
     )
 }
 
 #[inline(always)]
-pub fn clone<T: Num>(m: [T; 6]) -> [T; 6] {create(m[0], m[1], m[2], m[3], m[4], m[5])}
+pub fn clone<T: Num>(m: [T; 6]) -> [T; 6] {new(m[0], m[1], m[2], m[3], m[4], m[5])}
 
 #[inline(always)]
 pub fn copy<T: Num>(out: &mut [T; 6], a: [T; 6]) -> &mut [T; 6] {
